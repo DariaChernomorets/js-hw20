cb9186c4762963de242b057f9a056078178de3d9
Daria <101666130+DariaChernomorets@users.noreply.github.com>
2022-07-24 18:37:37 +0300

    1st try

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 'use strict';
 
-const costsOfPotato = function (borshchAmount = 48,potatosPerLiter = 4, potatoWeight = 75, potatoPrice = 13){
-  let totalWeight = borshchAmount * potatosPerLiter * (potatoWeight/1000);
-  let totalPrice = totalWeight * potatoPrice;
+const costsOfPotato = function (borshchAmount = 48,potatosPerLiter = 4, onePotatoWeight = 75, potatoPriceKg = 13){
+  if ( typeof borshchAmount !== 'number' || isNaN(borshchAmount)) throw new Error('Please enter how many borshch do you want to cook in liters');
+  if ( typeof potatosPerLiter !== 'number' || isNaN(potatosPerLiter)) throw new Error('Please enter how many potatoes do you use per liter in numbers');
+  if ( typeof onePotatoWeight !== 'number' || isNaN(onePotatoWeight)) throw new Error('Please enter valid weight of one potato ');
+  if ( typeof potatoPriceKg !== 'number' || isNaN(potatoPriceKg)) throw new Error('Please enter  valid price for 1 kg of potato ');
+
+  let totalWeight = borshchAmount * potatosPerLiter * (onePotatoWeight/1000);
+  let totalPrice = totalWeight * potatoPriceKg;
   return Math.round(totalPrice);
 
 }
 
 console.log(costsOfPotato());
+
